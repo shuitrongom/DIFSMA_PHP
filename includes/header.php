b2fc265d37255c2db7869b8273a7ead99eabd7b9
sergiohuitron <sergio.huitron@isol.dev>
2026-03-23 21:45:59 -0600

    se cambia logo de loading

--- includes/header.php
+++ includes/header.php
@@ -403,7 +403,10 @@ if (!isset($page_title)) $page_title = 'DIF San Mateo Atenco';
     <!-- Spinner Start -->
     <div id="spinner"
         class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-grow text-primary" role="status"></div>
+        <div class="spinner-wrapper">
+            <div class="spinner-ring"></div>
+            <img src="<?= $base_path ?>img/logo_DIF.png" alt="Cargando..." class="spinner-logo">
+        </div>
     </div>
     <!-- Spinner End -->
 
