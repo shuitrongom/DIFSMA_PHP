608f9c9bc34a41f14029dfb6bd3470445845c6a8
sergiohuitron <sergio.huitron@isol.dev>
2026-03-25 02:07:18 -0600

    se suebna rchvios de login

--- admin/login.php
+++ admin/login.php
@@ -229,6 +229,5 @@ $csrf = csrf_token();
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../js/upload-progress.js?v=13"></script>
 </body>
 </html>
