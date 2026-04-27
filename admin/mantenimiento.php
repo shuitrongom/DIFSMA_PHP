6a051ff6057b8c1bb568834eeddefd3e79b6bbda
difsmauser <difsmagithub@gmail.com>
2026-04-27 14:14:48 -0600

    se agrega acordeon

--- admin/mantenimiento.php
+++ admin/mantenimiento.php
@@ -400,6 +400,12 @@ require_once __DIR__ . '/page_help.php'; render_admin_sidebar($sidebar_groups, $
             </div>
         </div>
     </div>
-    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        // Inicializar acordeones manualmente si es necesario
+        document.addEventListener('DOMContentLoaded', function() {
+            console.log('Bootstrap cargado, acordeones listos');
+        });
+    </script>
 </body>
 </html>
